b76a7f65e8e4f2f2f52ef807602a1a0db4d3a1a5
Likholetov Kirill <shotykrd@yandex.ru>
2024-05-01 16:44:28 +0300

    fix: исправление pom, удаление импортов

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -1,11 +1,9 @@
 package ru.practicum.shareit.item;
 
 import org.mapstruct.Mapper;
-import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.UserMapper;
-import ru.practicum.shareit.user.dto.UserDto;
 
 @Mapper(componentModel = "spring", uses = UserMapper.class)
 public interface ItemMapper {
